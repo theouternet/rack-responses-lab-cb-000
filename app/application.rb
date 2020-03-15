4b22d63e9e5c238d6a62e62eb71bab0219ce2924
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-03-15 05:47:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,9 @@
 class Application
   
-  def Call 
+  def call(env)
      resp = Rack::Response.new
      now = time.now
-     if now.hour >= 12
+     if now >= 12
       resp.write "Good Morning!"
      else
       resp.write "Good Afternoon!"
